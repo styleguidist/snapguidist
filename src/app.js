2a9574204afe1de917ae9663be6c457e846beb57
Michele Bertoli <michele@berto.li>
2016-12-12 07:44:04 +0000

    add cors support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const snapguidist = require('./snapguidist')
 
 const app = express()
+
+app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
