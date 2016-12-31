3acab6f45751fe35e0d2527a44ed374a379987ae
Matteo Ronchi <matteo.ronchi@gmail.com>
2016-12-31 14:07:07 +0100

    Feat: Updated example to use the new config module

--- example/styleguide.config.js
+++ example/styleguide.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const snapguidist = require('../dist/snapguidist').default
 
 module.exports = {
   title: 'My Great Style Guide',
@@ -6,33 +7,8 @@ module.exports = {
   components: './src/**/*.js',
 
   updateWebpackConfig(webpackConfig) {
-    webpackConfig.module.loaders.push(
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: [
-            'babel-preset-es2015',
-            'babel-preset-react',
-          ].map(require.resolve),
-        },
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loaders: ['style', 'css'],
-      }
-    )
-
     webpackConfig.resolve.modulesDirectories = ['./example/node_modules', './node_modules']
-
-    webpackConfig.entry.push(path.join(__dirname, '../src/client/styles.css'))
-
-    webpackConfig.resolve.alias['rsg-components/Playground/PlaygroundRenderer'] = path.join(__dirname, '../src/client/PlaygroundRenderer')
-    webpackConfig.resolve.alias['rsg-components/Preview'] = path.join(__dirname, '../src/client/Preview')
-
-    return webpackConfig
+    return snapguidist(webpackConfig)
   },
 
 }
