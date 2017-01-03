f8ef15c4d431ceaa0bde2f96f556b0f16ee83b06
Matteo Ronchi <matteo.ronchi@gmail.com>
2017-01-03 01:08:57 +0100

    Refactor: Updated example to work properly both standalone and as snapguidist dev environment

--- example/styleguide.config.js
+++ example/styleguide.config.js
@@ -1,14 +1,30 @@
-const snapguidist = require('../dist/index').default
+const path = require('path')
+
+/* eslint-disable */
+let snapguidist
+if (process.env.SNAPGUIDIST_MODE === 'dev') {
+  snapguidist = require(path.join(process.cwd(), 'src', 'index'))
+} else {
+  snapguidist = require('snapguidist')
+}
+/* eslint-enable */
+
 
 module.exports = snapguidist({
-  title: 'My Great Style Guide',
+  title: 'Snapguidist Styleguide',
 
-  components: './src/**/*.js',
+  components: 'src/components/**/[A-Z]*.js',
 
   updateWebpackConfig(webpackConfig) {
     webpackConfig.devtool = 'source-map'
-    webpackConfig.resolve.modulesDirectories = ['./example/node_modules', './node_modules']
+
+    const sourceFolder = path.resolve(__dirname, 'src')
+    webpackConfig.module.loaders.push({
+      test: /\.jsx?$/,
+      include: sourceFolder,
+      loader: 'babel',
+    })
+
     return webpackConfig
   },
-
 })
