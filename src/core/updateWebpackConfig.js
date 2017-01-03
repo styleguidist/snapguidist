ef4f0e07e2299d4c41d22bb911c915b27d0963d6
Matteo Ronchi <matteo.ronchi@gmail.com>
2017-01-03 20:37:50 +0100

    Refactor: Removed need of an environment variable to run DEV mode

--- src/core/updateWebpackConfig.js
+++ src/core/updateWebpackConfig.js
@@ -1,37 +1,30 @@
 const path = require('path')
 const webpack = require('webpack')
 
-let base = path.join(process.cwd(), 'node_modules', 'snapguidist')
-if (process.env.SNAPGUIDIST_MODE === 'dev') {
-  base = process.cwd()
-}
-const componentPath = component => path.join(base, 'src', 'components', component)
-const stylesPath = style => path.join(base, 'src', style)
+const srcFolder = path.join(__dirname, '..')
+const componentPath = component => path.join(srcFolder, 'components', component)
+const stylesPath = style => path.join(srcFolder, style)
 
 const PLAYGROUND_RENDERER = 'rsg-components/Playground/PlaygroundRenderer'
 const PLAYGROUND_PREVIEW = 'rsg-components/Preview'
 
 function updateWebpackConfig(webpackConfig, env, serverInfo) {
-  const includePaths = [
-    path.join(base, 'src'),
-  ]
-
   webpackConfig.module.loaders.push(
     {
       test: /\.jsx?$/,
-      include: includePaths,
+      include: srcFolder,
       loader: 'babel',
       query: {
         presets: [
           'babel-preset-es2015',
           'babel-preset-react',
           'babel-preset-stage-0',
-        ].map(require.resolve),
+        ],
       },
     },
     {
       test: /\.css$/,
-      include: includePaths,
+      include: srcFolder,
       loaders: ['style', 'css'],
     }
   )
