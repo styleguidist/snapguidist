96b3f336f51eb275b80b2261630185af63f0c1c0
Matteo Ronchi <matteo.ronchi@gmail.com>
2017-01-03 20:38:35 +0100

    Refactor: Improved example to expose 2 different styleguide config files for dev and normal mode

--- example/styleguide.config.dev.js
+++ example/styleguide.config.dev.js
@@ -1,12 +1,15 @@
 const path = require('path')
-const snapguidist = require('snapguidist')
+const snapguidist = require(path.join(__dirname, '..', 'src', 'index'))
+
 
 module.exports = snapguidist({
-  title: 'Snapguidist Styleguide',
+  title: 'Snapguidist Development Example',
 
   components: 'src/components/**/[A-Z]*.js',
 
   updateWebpackConfig(webpackConfig) {
+    webpackConfig.devtool = 'source-map'
+
     const sourceFolder = path.resolve(__dirname, 'src')
     webpackConfig.module.loaders.push({
       test: /\.jsx?$/,
