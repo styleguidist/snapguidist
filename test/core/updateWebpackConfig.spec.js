9f93c10e61a7d5201f085882024ffde361e8c192
Michele Bertoli <michele@berto.li>
2017-01-04 11:42:53 +0000

    test updateWebpackConfig

---
+++ test/core/updateWebpackConfig.spec.js
@@ -0,0 +1,52 @@
+import path from 'path'
+import updateWebpackConfig from '../../src/core/updateWebpackConfig'
+
+jest.mock('webpack', () => ({
+  DefinePlugin(data) { return { definitions: data } },
+}))
+
+const srcFolder = path.join(__dirname, '..', '..')
+
+test('enhances the webpack configuration', () => {
+  const webpackConfig = {
+    entry: [],
+    resolve: { alias: {} },
+    module: { loaders: [] },
+    plugins: [],
+  }
+  const serverInfo = 'serverInfo'
+
+  const expected = {
+    entry: [`${srcFolder}/src/styles.css`],
+
+    module: {
+      loaders: [{
+        include: `${srcFolder}/src`,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+        test: /\.jsx?$/,
+      }, {
+        include: `${srcFolder}/src`,
+        loaders: ['style', 'css'],
+        test: /\.css$/,
+      }],
+    },
+
+    plugins: [{
+      definitions: {
+        'process.env.SNAPGUIDIST': '"serverInfo"',
+      },
+    }],
+
+    resolve: {
+      alias: {
+        'rsg-components/Playground/PlaygroundRenderer': `${srcFolder}/src/components/PlaygroundRenderer`,
+        'rsg-components/Preview': `${srcFolder}/src/components/Preview`,
+      },
+    },
+  }
+
+  expect(updateWebpackConfig(webpackConfig, null, serverInfo)).toEqual(expected)
+})
