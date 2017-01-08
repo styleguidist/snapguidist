5ac1d3f0904542efd7b337304331d93ef772b371
Matteo Ronchi <matteo.ronchi@gmail.com>
2017-01-08 16:34:03 +0100

    Style: Removed comments from code and moved to function definition

--- src/context/snapguidistContextFactory.js
+++ src/context/snapguidistContextFactory.js
@@ -12,26 +12,28 @@ export default function snapguidistContextFactory() {
     cache.clear()
   }
 
+  /**
+   * It creates the component snapshot, already in JSON format and
+   * then check if the snapshot is changed from the one stored from last execution.
+   *
+   * @param {string} name - Unique name of the component to be tested
+   * @param {ReactElement} component - ReactElement of the compoennt to be tested
+   * @param {boolean} [update] - It specifies if the test must check or update the stored snapshot
+   * @return {boolean} Returns true if the test is run, false otherwise
+   */
   const runTest = (name, component, update) => {
-    // create the component snapshot, already in JSON format
     const snapshot = renderSnapshot(name, component)
 
-    // check if the snapshot is changed from the stored one, if exists
     if (!update && cache.has(name)) {
       const { snapshot: storedSnapshot } = cache.get(name)
       if (snapshot === storedSnapshot) {
-        // return false if the test has not been run
         return false
       }
     }
 
-    // update snapshot cache
     cache.set(name, { name, snapshot })
-
-    // add the test to the execution queue
     queue.addTest(name, snapshot, update)
 
-    // return true if the test has not been run
     return true
   }
 
@@ -40,8 +42,13 @@ export default function snapguidistContextFactory() {
     return () => listeners.delete(listener)
   }
 
-  // listen for queue event
-  queue.listen(({ name, response }) => listeners.forEach(listener => listener({ name, response })))
+  const init = () => {
+    queue.listen(({ name, response }) => listeners.forEach(
+      listener => listener({ name, response })
+    ))
+  }
+
+  init()
 
   return {
     clear,
