9ffdeb80b0bb30237878fb1517c1f496b538ba6d
Josh Johnston <josh@x-team.com>
2017-11-15 21:20:36 +1100

    Prevent crashing when a component is null (#26)
    * failing specs
    
    * updates to pass specs
    
    * fixed typo in spec

--- test/core/snapshot.spec.js
+++ test/core/snapshot.spec.js
@@ -36,3 +36,49 @@ test('does not fail if update is true', () => {
 
   expect(result).toEqual({ pass: true })
 })
+
+test('passes if null, first time', () => {
+  const tree = null
+  const result = snapshot('name', tree)
+
+  expect(result).toEqual({ pass: true })
+})
+
+test('passes if new and old are null', () => {
+  const name = 'both-null'
+  snapshot(name, null)
+  const result = snapshot(name, null)
+
+  expect(result).toMatchSnapshot()
+})
+
+test('fails if old was null but new is not', () => {
+  const name = 'old-was-null'
+  snapshot(name, null)
+
+  const tree = { type: 'div' }
+  const result = snapshot(name, tree)
+
+  expect(result).toMatchSnapshot()
+})
+
+test('fails if new is null but old is not', () => {
+  const name = 'new-is-null'
+  const tree = { type: 'div' }
+  snapshot(name, tree)
+
+  const result = snapshot(name, null)
+
+  expect(result).toMatchSnapshot()
+})
+
+test('null tree does not fail if update is true', () => {
+  const name = 'null-with-update'
+  const tree = { type: 'div' }
+  snapshot(name, tree)
+
+  const update = true
+  const result = snapshot(name, null, update)
+
+  expect(result).toEqual({ pass: true })
+})
